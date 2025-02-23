efefbc62cca50168f4d449487a2c186d81f88c40
VocaTea <russulgin@gmail.com>
2025-02-23 15:49:16 +1200

    Create Рулетка.py
    It's a simple module, its job is that it randomly selects 1 photo and sends it to your chat, where you use this Module.

---
+++ Рулетка.py
@@ -0,0 +1,44 @@
+#commands: nlh
+__version__ = (1 , 0)
+import os
+import logging
+from .. import loader, utils
+import random
+import time
+import datetime
+from telethon import functions
+from telethon.tl.custom import Message
+
+logger = logging.getLogger("рулетка")
+
+@loader.tds
+class рулетка(loader.Module):
+    """Anime arts roulet"""
+    strings = {
+        "name": "Рулетка",
+        "search": <emoji document_id=5188311512791393083>🔎</emoji> search your art..."
+    }
+    
+    async def nlhcmd(self, message: Message):
+         """-> to get your arts"""
+         await message.edit(self.strings("search"))
+         time.sleep(0.5)
+         chat = "Kkkain"
+         result = await message.client(
+             functions.messages.GetHistoryRequest(
+                 peer=chat,
+                 offset_id=0,
+                 offset_date=datetime.datetime.now(),
+                 add_offset=random.choice(range(1, 1420, 4)),
+                 limit=1,
+                 max_id=0,
+                 min_id=0,
+                 hash=0,
+             ),
+         )
+         await message.delete()
+         await message.client.send_file(
+             message.to_id, 
+             result.messages[0].media, 
+             reply_to=getattr(message, "reply_to_msg_id", None),
+             )
